5ba7e7f5d137fbc7f7a18a7f4475c05f7f55eecc
Becca <rrdoukas@gmail.com>
2020-07-22 12:50:38 -0500

    adds logout and updates capstone show

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> | 
-      <router-link to="/login">Login</router-link> |
+      <router-link to="/login">Login</router-link> | 
+      <router-link to="/logout">Logout</router-link> | 
       <router-link to="/resumes/:id">Resume</router-link> |
       <router-link to="/capstones/:id">Capstone</router-link>
     </div>
--- src/views/CapstonesShow.vue
+++ src/views/CapstonesShow.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="capstones-show">
-    <h1>CAPSTONES!</h1>
+    <h1>Your Capstone:</h1>
+    <h2>Plant Swapp</h2>
+    <h2>A place to share plants with other plant lovers!</h2>
+    <img v-bind:src="capstone.screenshot_url" v-bind:all="capstone.name" />
+    <h2>{{capstone.name}}</h2>
+    <h2>{{capstone.description}}</h2>
+    <h2>{{capstone.url}}</h2>
   </div>
 </template>
 
@@ -8,9 +14,22 @@
 import axios from "axios";
 
 export default {
-  data: function() {
-    return {};
+  data: function () {
+    return {
+      description: "Plant Swapp App",
+      url: "http:capstone.com",
+      name: "Plant Swapp",
+      screenshot_url: "screenshot",
+      // student_id: "",
+      errors: [],
+    };
   },
-  methods: {}
+  created: function () {
+    axios.get(`/api/capstones/${this.$route.params.id}`).then((response) => {
+      console.log("capstones show", response);
+      this.capstone = response.data;
+    });
+  },
+  methods: {},
 };
 </script>
