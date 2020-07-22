2593d38bbdc7f613501d5553f71a759cb0644cbf
carlhauck <carlhauck@gmail.com>
2020-07-22 11:34:24 -0500

    adds ResumesShow and CapstonesShow views

---
+++ src/views/CapstonesShow.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="capstones-show">
+    <h1>CAPSTONES!</h1>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {};
+  },
+  methods: {}
+};
+</script>
